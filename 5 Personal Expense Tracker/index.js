ef2b6d5d244ea3492d8c74e43311a94158eea928
Brioleays <brightfuturedev2025@gmail.com>
2026-08-31 12:47:26 +0100

    project 5

---
+++ 5 Personal Expense Tracker/index.js
@@ -0,0 +1,14 @@
+state = {
+    expenses: [
+        {
+            //id: Date.now(),
+            // date: '2026-08-28',
+            // category: 'Food',
+            // amount: 5000,
+            // recipient: 'Mama Put'
+        }
+    ],
+    status: 'empty', //'empty'|'loaded'|'filtered'|'loading'
+    exchangeRate: {}, //naira: 1, usd: 1600 
+    currentFilter: 'all' //all | recipient |category
+};
